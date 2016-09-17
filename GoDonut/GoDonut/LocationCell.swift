ec0b51f5492314d4b5a2d0881a490d36398d3b7b
Brandon Ballentine <brandon.ballentine@gmail.com>
2016-09-16 17:01:13 -0700

    Initial commit

---
+++ GoDonut/GoDonut/LocationCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationCell.swift
+//  GoDonut
+//
+//  Created by Brandon Ballentine on 9/16/16.
+//  Copyright © 2016 Brandon Ballentine. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
