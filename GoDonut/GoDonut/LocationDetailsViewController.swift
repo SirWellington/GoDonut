ec8266d7236875c8021c5c45be2c5b17dd7b4f79
Brandon Ballentine <brandon.ballentine@gmail.com>
2016-09-22 17:06:28 -0700

    Adjust UI for displaying reviews

--- GoDonut/GoDonut/LocationDetailsViewController.swift
+++ GoDonut/GoDonut/LocationDetailsViewController.swift
@@ -119,6 +119,7 @@ class LocationDetailsViewController: UITableViewController {
 
     }
     
+    
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         if section == 0 {
             return "Customer Reviews"
